feab1cac680133166de2faa110090a565656deca
Gustavob89 <76412104+Gustavob89@users.noreply.github.com>
2020-12-20 21:18:12 -0300

    Add files via upload

---
+++ Prototypes/Initial_2020_12_20/Firmware/usb_dev_bulk/Debug/usb_bulk_structs.d
@@ -0,0 +1,39 @@
+# FIXED
+
+usb_bulk_structs.obj: ../usb_bulk_structs.c
+usb_bulk_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h
+usb_bulk_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h
+usb_bulk_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h
+usb_bulk_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h
+usb_bulk_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h
+usb_bulk_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h
+usb_bulk_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h
+usb_bulk_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h
+usb_bulk_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h
+usb_bulk_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+usb_bulk_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/usb.h
+usb_bulk_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usblib.h
+usb_bulk_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usb-ids.h
+usb_bulk_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevice.h
+usb_bulk_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevicepriv.h
+usb_bulk_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdbulk.h
+usb_bulk_structs.obj: ../usb_bulk_structs.h
+
+../usb_bulk_structs.c: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/usb.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usblib.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usb-ids.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevice.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevicepriv.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdbulk.h: 
+../usb_bulk_structs.h: 
---
+++ Prototypes/Initial_2020_12_20/Firmware/usb_dev_bulk/Debug/usb_dev_bulk.d
@@ -0,0 +1,83 @@
+# FIXED
+
+usb_dev_bulk.obj: ../usb_dev_bulk.c
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdlib.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/fpu.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usblib.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usb-ids.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevice.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevicepriv.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdbulk.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdarg.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/time.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/abi_prefix.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/xlocale/_time.h
+usb_dev_bulk.obj: ../usb_bulk_structs.h
+usb_dev_bulk.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h
+usb_dev_bulk.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/string.h
+
+../usb_dev_bulk.c: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdlib.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/fpu.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usblib.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usb-ids.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevice.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevicepriv.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdbulk.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdarg.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/time.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/abi_prefix.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/xlocale/_time.h: 
+../usb_bulk_structs.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/string.h: 
